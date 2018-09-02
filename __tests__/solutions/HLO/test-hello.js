3e5925689b7df550de4639ebd64ee2471e9c605f
sbx_user1060 <sbx_user1060@ip-10-128-182-118.eu-west-2.compute.internal>
2018-09-02 11:05:05 +0000

    Sun Sep 02 11:05:05 UTC 2018

--- __tests__/solutions/HLO/test-hello.js
+++ __tests__/solutions/HLO/test-hello.js
@@ -3,5 +3,9 @@ const hello = require('../../../lib/solutions/HLO/hello');
 describe('hello', () => {
   it('returns a string containing the friend name', () => {
     expect(hello('john')).toEqual('Hello, john!');
-  })
-})
+  });
+
+  it('returns a string containing the input friends name', () => {
+    expect(hello('Lucy')).toEqual('Hello, Lucy!');
+  });
+});

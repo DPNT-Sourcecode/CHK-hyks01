46c5912f5c7aafd9e25d82579206731449a27710
sbx_user1060 <sbx_user1060@ip-10-128-182-118.eu-west-2.compute.internal>
2018-09-02 11:14:21 +0000

    Sun Sep 02 11:14:21 UTC 2018

--- __tests__/solutions/CHK/test-checkout.js
+++ __tests__/solutions/CHK/test-checkout.js
@@ -1,3 +1,5 @@
+const checkout = require('../../../lib/solutions/CHK/checkout');
+
 describe('checkout', () => {
   it('returns -1 for any illegal input', () => {
     expect(checkout('illegal input')).toEqual(-1);

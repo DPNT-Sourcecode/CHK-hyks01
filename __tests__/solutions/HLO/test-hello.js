fe1254ede60e58f5c4f332579f4226815a327072
sbx_user1060 <sbx_user1060@ip-10-128-182-118.eu-west-2.compute.internal>
2018-09-02 10:57:00 +0000

    Sun Sep 02 10:57:00 UTC 2018

--- __tests__/solutions/HLO/test-hello.js
+++ __tests__/solutions/HLO/test-hello.js
@@ -1,1 +1,7 @@
 const hello = require('../../../lib/solutions/HLO/hello');
+
+describe('hello', () => {
+  it('returns a string', () => {
+    expect(hello('some string')).toEqual('some string');
+  })
+})

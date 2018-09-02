701b3e899d8e7386ecca1e77810c3621941aa31a
sbx_user1060 <sbx_user1060@ip-10-128-182-118.eu-west-2.compute.internal>
2018-09-02 11:26:21 +0000

    Sun Sep 02 11:26:21 UTC 2018

--- __tests__/solutions/CHK/test-checkout.js
+++ __tests__/solutions/CHK/test-checkout.js
@@ -12,4 +12,12 @@ describe('checkout', () => {
   it('returns the cost of item B', () => {
     expect(checkout('B')).toEqual(30);
   });
+
+  it('returns the cost of item C', () => {
+    expect(checkout('C')).toEqual(20);
+  });
+
+  it('returns the cost of item D', () => {
+    expect(checkout('D')).toEqual(15);
+  });
 });
